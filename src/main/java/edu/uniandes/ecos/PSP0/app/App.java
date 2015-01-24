90976366a7fd374ffa844208e6687d89b58c62f6
sebascardonac11 <sebascardonac11@gmail.com>
2015-01-24 10:17:39 -0500

    Segunda versión del proyecto adicionando e ignorando más archivos

---
+++ src/main/java/edu/uniandes/ecos/PSP0/app/App.java
@@ -0,0 +1,14 @@
+package edu.uniandes.ecos.PSP0.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Sebastian Cardona Correa");
+    }
+}
